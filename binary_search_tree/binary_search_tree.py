8789c2e96a81f5d4cb5f7a8a6b421222c695c588
Craig Swanson <cswanson02@me.com>
2020-09-20 22:12:51 -0500

    Implemented insert() and contains() in binary_serch_tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,12 +17,48 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # take the current value of our node (self.value)
+        # compare it to the value we want to insert
+        
+        # new value < self.value
+            # if self.left is already taken by a node
+                # make that node call insert
+            # set the left child to the new node with the new value
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        
+        # if the new value >= self.value
+            # if self.right is already taken by a node
+                # make that node call insert
+            # set the right child to the new node with new value
+        if value >= self.value:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+        # compare the target to current value
+        # if current value < target
+        if self.value < target:
+            if self.right is None:
+                return False
+            found = self.right.contains(target)
+
+        if self.value >= target:
+            #check the left subtree
+            if self.left is None:
+                return False
+            found = self.left.contains(target)
+        
+        return found
 
     # Return the maximum value found in the tree
     def get_max(self):
@@ -80,6 +116,6 @@ print("elegant methods")
 print("pre order")
 bst.pre_order_dft()
 print("in order")
-bst.in_order_dft()
+# bst.in_order_dft()
 print("post order")
 bst.post_order_dft()  
